19e2dfa2216166be6020efbfd4cba199cb9d49f7
Marianne Rakic <mrakic@mit.edu>
2021-11-10 15:39:22 -0500

    plot functions specially for brain, OASIS data

--- plot/plt_brain.py
+++ plot/plt_brain.py
@@ -1,1 +1,18 @@
+import numpy as np
+import neurite as ne
+
+
+
+def ne_plot_slices(array, titles=None, seg=False):
+
+    if titles is None:
+        titles = ["" for i in range(len(array))]
+
+    if seg:
+        img = [np.rot90(np.argmax(i, axis=-1), -1) for i in array]
+        ne.plot.slices(img, titles = titles)
+
+    else:
+        img = [np.rot90(i, -1) for i in array]
+        ne.plot.slices(img, titles = titles, cmaps=['gray'], do_colorbars=True)
 
